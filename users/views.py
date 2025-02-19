5ae546ed40a2ab490973c6df3893d76f03f308e1
Walter Lira | Maud Capital <wlira@maud.capital>
2025-02-19 15:00:40 -0300

    ajuste tela login

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_protect,csrf_exempt
 from .forms import LoginForm
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import logout
 
-@csrf_protect
+@csrf_exempt
 def login(request):
     message = None
     if request.method != 'POST':
