6584550c7429aba995b47f35289b166f181860af
Walter Lira | Maud Capital <wlira@maud.capital>
2025-04-14 09:55:52 -0300

    file static

--- _dashboard/settings.py
+++ _dashboard/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'common','static'),)
 
 # Default primary key field type
