c206b65b50a430d37a3b39dc781ce78bfa4f8baf
Walter Lira | Maud Capital <wlira@maud.capital>
2025-04-15 10:44:05 -0300

    date table

--- common/signals.py
+++ common/signals.py
@@ -5,29 +5,13 @@ from django.db import connection
 from django.dispatch import receiver
 
 @receiver(post_migrate)
-def alterar_timestamp_sem_timezone(sender, **kwargs):
-    def precisa_alterar(coluna):
-        with connection.cursor() as cursor:
-            cursor.execute(f"""
-                SELECT data_type, datetime_precision
-                FROM information_schema.columns
-                WHERE table_name = 'common_datadashboard'
-                AND column_name = %s;
-            """, [coluna])
-            result = cursor.fetchone()
-            if not result:
-                return False
-            tipo = result[0]
-            return tipo == 'timestamp with time zone'
+def testar_alter_column_timestamp(sender, **kwargs):
+    with connection.cursor() as cursor:
+        print("[Signal] Rodando alteração de tipo só para teste...")
 
-    print("[Signal] Verificação de tipos de timestamp executada.")
-
-    colunas = ['creation_date', 'ref_date']
-    if any(precisa_alterar(coluna) for coluna in colunas):
-        with connection.cursor() as cursor:
-            print("[Signal] Alterando tipo de coluna para timestamp WITHOUT time zone...")
-            cursor.execute("""
-                ALTER TABLE common_datadashboard
-                ALTER COLUMN creation_date TYPE timestamp WITHOUT time zone,
-                ALTER COLUMN ref_date TYPE timestamp WITHOUT time zone;
-            """)
+        cursor.execute("""
+            ALTER TABLE common_datadashboard
+            ALTER COLUMN creation_date TYPE timestamp(0) WITHOUT time zone,
+            ALTER COLUMN ref_date TYPE timestamp(0) WITHOUT time zone;
+        """)
+    
