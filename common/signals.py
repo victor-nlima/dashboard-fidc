66ee0f65235b4c84efdcba5e2b53c571984e7775
Walter Lira | Maud Capital <wlira@maud.capital>
2025-04-15 10:46:54 -0300

    error

--- common/signals.py
+++ common/signals.py
@@ -1,17 +1,17 @@
-# common/signals.py
+# # common/signals.py
 
-from django.db.models.signals import post_migrate
-from django.db import connection
-from django.dispatch import receiver
+# from django.db.models.signals import post_migrate
+# from django.db import connection
+# from django.dispatch import receiver
 
-@receiver(post_migrate)
-def testar_alter_column_timestamp(sender, **kwargs):
-    with connection.cursor() as cursor:
-        print("[Signal] Rodando alteração de tipo só para teste...")
+# @receiver(post_migrate)
+# def testar_alter_column_timestamp(sender, **kwargs):
+#     with connection.cursor() as cursor:
+#         print("[Signal] Rodando alteração de tipo só para teste...")
 
-        cursor.execute("""
-            ALTER TABLE common_datadashboard
-            ALTER COLUMN creation_date TYPE timestamp(0) WITHOUT time zone,
-            ALTER COLUMN ref_date TYPE timestamp(0) WITHOUT time zone;
-        """)
+#         cursor.execute("""
+#             ALTER TABLE common_datadashboard
+#             ALTER COLUMN creation_date TYPE timestamp(0) WITHOUT time zone,
+#             ALTER COLUMN ref_date TYPE timestamp(0) WITHOUT time zone;
+#         """)
     
